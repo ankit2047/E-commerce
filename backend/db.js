779813036bec44ba809c435307b160d2538153d3
ankit2047 <ankit31181gupta@gmail.com>
2024-12-27 18:35:21 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose");
+const DB=process.env.DATABASE;
 
-mongoose.connect("mongodb://localhost:27017/shiwansh1").then(()=>{
+
+mongoose.connect(DB).then(()=>{
     console.log("Database Connect Successfully");
 }).catch((error)=>{
     console.log("Database not connected",error);
